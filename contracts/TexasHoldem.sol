0237624632daa8e1ea1ea9e6a4a710c08f7abea5
Zsofie <zsofia@linumlabs.com>
2024-05-08 10:40:12 -0700

    .

--- contracts/TexasHoldem.sol
+++ contracts/TexasHoldem.sol
@@ -159,7 +159,211 @@ function createTable(TableState _state,uint _buyInAmount, uint _maxPlayers,uint[
     return tableCount;
 }
 
-function createPlayer(address _wallet, uint tableID) external returns(uint) {
+address[] playerAddresses; // declaing storage variable for the function below
+
+function openRound(uint tableID, uint playerID) external onlyOwner {
+    // opens a new round in a game
+
+    uint roundCount;
+
+    unchecked {
+        roundCount++;
+    }
+    
+    if(roundCount == 1) {
+            //taking initial bets
+
+    uint[] memory playerIDs = tables[tableID].players;
+
+    // TODO
+    for (uint i = 0; i < playerIDs.length; i++) { 
+         
+        playerAddresses.push(players[playerIDs[i]].wallet);
+         for (uint j = 0; i < playerAddresses.length; j++) {
+            IERC20(tables[tableID].token).transferFrom(playerAddresses[i],tables[tableID].pot,tables[tableID].buyInAmount);
+         }
+    }
+
+    // set totalAmountInPot 
+
+    uint potBalance = IERC20(tables[tableID].token).balanceOf(tables[tableID].pot);
+    tables[tableID].totalAmountinPot = potBalance;
+
+    // deal cards to players and place community cards on the table
+    _dealCards();
+    _dealCommunityCards();
+    }
+
+    if (roundCount > 1) {
+      _addCommunityCard();
+    }
+
+    emit RoundOpened(tableID,roundCount);
+
+}
+
+function playerAction(PlayerAction action, uint raiseAmount, uint tableID, uint playerID) external onlyActivePlayer(playerID) {
+    if(msg.sender != players[playerID].wallet) revert OnlyRegisteredPlayer("");
+
+    if (action == PlayerAction.Call) {
+    // player puts the amount to what's in the pot already
+    
+    // update totalAmountinPot
+        uint currentAmount = tables[tableID].totalAmountinPot;
+        uint newAmount = currentAmount * 2 ;
+        currentAmount = newAmount;
+
+    // trasfer funds to pot
+      IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,tables[tableID].totalAmountinPot);
+       emit PlayerCalled(playerID);
+       
+    }
+
+    if (action == PlayerAction.Raise) {
+    // player raises the last highest chip
+    if(raiseAmount < tables[tableID].totalAmountinPot) revert BidTooLow("insufficient bid");
+      
+      // update bets in pot
+        uint currentAmount = tables[tableID].totalAmountinPot;
+        uint newAmount = currentAmount + raiseAmount;
+        currentAmount = newAmount;
+     
+     // transfer funds
+        IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,raiseAmount);
+        emit PlayerRaised(playerID);
+        
+    }
+
+    if (action == PlayerAction.Check) {
+    // player does nothing
+        emit PlayerChecked(playerID);
+    }
+
+    if (action == PlayerAction.Fold) {
+    // player reveals cards and gets removed from active players
+
+        // TODO revealCards();
+
+        // set player to inactive
+        players[playerID].isActivePlayer = false;
+        emit PlayerFolded(playerID);
+    }
+}
+
+function showdown(uint tableID) external onlyOwner {
+    // all active players reveal their cards
+
+     uint[] memory playerIDs = tables[tableID].players;
+     for (uint i = 0; i < playerIDs.length; i++) {
+        // TODO revealCards(palyerIDs[i]);
+     }
+   
+    uint playerID;
+   // = determineWinner(); TODO - return playerID via this function
+   _closeTable(tableID,playerID);
+}
+
+function determineWinner(uint tableID) external onlyOwner returns (uint){
+    // TODO function that determins the winner and returns playerID
+}
+
+function blacklistPlayer(uint playerID) external onlyOwner{
+    // blacklists a malicious player
+
+    address playerAddress = players[playerID].wallet;
+    if (balcklistedAddress[playerAddress]) revert AddressBlacklisted("address already blacklisted");
+
+    players[playerID].isBlacklisted = true;
+    
+    balcklistedAddress[playerAddress] = true;
+
+    emit PlayerBlacklisted(players[playerID].wallet);
+}
+
+// -------------------------------------------------------------
+// INTERNAL FUNCTIONS
+// -------------------------------------------------------------
+
+   function _dealCards() internal {
+    // TODO
+    // assuming that the cards are NFTs we can integrate Chainlink's VFR function and deal random tokenIDs to each player and send it to their wallet
+    // we would save the corresponding 
+   }
+
+   function _dealCommunityCards() internal {
+    // TODO
+    // assuming that the cards are NFTs we can integrate Chainlink's VFR function deal random cards by a random tokenID to the table
+   }
+
+   function _addCommunityCard() internal {
+    // TODO
+    // assuming that the cards are NFTs we can integrate Chainlink's VFR function and deal a random card by a random tokenID to the table
+   }
+
+   function _revealCards(uint playerID) internal view returns (uint8[] memory cards) {
+    return players[playerID].cards;
+   }
+
+   function _closeTable(uint tableID,uint playerID) internal {
+    if (tables[tableID].state == TableState.Inactive) revert TableAlreadyClosed(tableID);
+
+    // close the table
+    tables[tableID].state = TableState.Inactive;
+    tables[tableID].winner = players[playerID].wallet;
+
+    // reset all players to active
+    uint[] memory playerIDs = tables[tableID].players;
+    for (uint i = 0; i < playerIDs.length; i++) { 
+        _setPlayerToActive(playerIDs[i]);
+    }
+
+     // TODO send cards back to dealer wallet
+
+    // for (uint i = 0; i < playerIDs.length; i++) { 
+    //     IERC1155(tables[tableID].cardsAddress).transferFrom(playerWallets[i],address(this),tokenIDs[i]);
+    // }
+    
+    _sendWinnerRewards(tableID,playerID,tables[tableID].totalAmountinPot);
+    emit TableClosed(tables[tableID].winner,tables[tableID].totalAmountinPot);
+}
+
+function _sendWinnerRewards(uint tableID,uint playerID, uint amount) internal {
+
+     // set totalAmountInPot to 0
+
+     tables[tableID].totalAmountinPot = 0;
+
+     IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,amount);
+
+     emit RewardsTransferred(players[playerID].wallet,amount);
+}
+
+function _setPlayerToActive(uint playerID) internal {
+    players[playerID].isActivePlayer = true;
+}
+
+// -------------------------------------------------------------
+// CHAINLINK
+// -------------------------------------------------------------
+
+function fulfillRandomWords(
+        uint256 _requestId,
+        uint256[] memory _randomWords
+    ) internal override {
+        // TODO function to get the random numbers
+    }
+
+      function getRequestStatus(uint256 _requestId)
+        external
+        view
+        returns (bool fulfilled, uint256[] memory randomWords)
+    {
+        require(s_requests[_requestId].exists, "request not found");
+        RequestStatus memory request = s_requests[_requestId];
+        return (request.fulfilled, request.randomWords);
+    }
+
+    function createPlayer(address _wallet, uint tableID) external returns(uint) {
     // registers a player at a table with an ID
     uint[] memory playerArray = tables[tableID].players;
     if (balcklistedAddress[_wallet]) revert AddressBlacklisted("can't register this address");
@@ -178,209 +382,5 @@ function createPlayer(address _wallet, uint tableID) external returns(uint) {
 
    emit PlayerCreated(playerCount);
 }
-
-address[] playerAddresses; // declaing storage variable for the function below
-
-// function openRound(uint tableID, uint playerID) external onlyOwner {
-//     // opens a new round in a game
-
-//     uint roundCount;
-
-//     unchecked {
-//         roundCount++;
-//     }
-    
-//     if(roundCount == 1) {
-//             //taking initial bets
-
-//     uint[] memory playerIDs = tables[tableID].players;
-
-//     // TODO
-//     for (uint i = 0; i < playerIDs.length; i++) { 
-         
-//         playerAddresses.push(players[playerIDs[i]].wallet);
-//          for (uint j = 0; i < playerAddresses.length; j++) {
-//             IERC20(tables[tableID].token).transferFrom(playerAddresses[i],tables[tableID].pot,tables[tableID].buyInAmount);
-//          }
-//     }
-
-//     // set totalAmountInPot 
-
-//     uint potBalance = IERC20(tables[tableID].token).balanceOf(tables[tableID].pot);
-//     tables[tableID].totalAmountinPot = potBalance;
-
-//     // deal cards to players and place community cards on the table
-//     _dealCards();
-//     _dealCommunityCards();
-//     }
-
-//     if (roundCount > 1) {
-//       _addCommunityCard();
-//     }
-
-//     emit RoundOpened(tableID,roundCount);
-
-// }
-
-// function playerAction(PlayerAction action, uint raiseAmount, uint tableID, uint playerID) external onlyActivePlayer(playerID) {
-//     if(msg.sender != players[playerID].wallet) revert OnlyRegisteredPlayer("");
-
-//     if (action == PlayerAction.Call) {
-//     // player puts the amount to what's in the pot already
-    
-//     // update totalAmountinPot
-//         uint currentAmount = tables[tableID].totalAmountinPot;
-//         uint newAmount = currentAmount * 2 ;
-//         currentAmount = newAmount;
-
-//     // trasfer funds to pot
-//       IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,tables[tableID].totalAmountinPot);
-//        emit PlayerCalled(playerID);
-       
-//     }
-
-//     if (action == PlayerAction.Raise) {
-//     // player raises the last highest chip
-//     if(raiseAmount < tables[tableID].totalAmountinPot) revert BidTooLow("insufficient bid");
-      
-//       // update bets in pot
-//         uint currentAmount = tables[tableID].totalAmountinPot;
-//         uint newAmount = currentAmount + raiseAmount;
-//         currentAmount = newAmount;
-     
-//      // transfer funds
-//         IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,raiseAmount);
-//         emit PlayerRaised(playerID);
-        
-//     }
-
-//     if (action == PlayerAction.Check) {
-//     // player does nothing
-//         emit PlayerChecked(playerID);
-//     }
-
-//     if (action == PlayerAction.Fold) {
-//     // player reveals cards and gets removed from active players
-
-//         // TODO revealCards();
-
-//         // set player to inactive
-//         players[playerID].isActivePlayer = false;
-//         emit PlayerFolded(playerID);
-//     }
-// }
-
-// function showdown(uint tableID) external onlyOwner {
-//     // all active players reveal their cards
-
-//      uint[] memory playerIDs = tables[tableID].players;
-//      for (uint i = 0; i < playerIDs.length; i++) {
-//         // TODO revealCards(palyerIDs[i]);
-//      }
-   
-//     uint playerID;
-//    // = determineWinner(); TODO - return playerID via this function
-//    _closeTable(tableID,playerID);
-// }
-
-// function determineWinner(uint tableID) external onlyOwner returns (uint){
-//     // TODO function that determins the winner and returns playerID
-// }
-
-// function blacklistPlayer(uint playerID) external onlyOwner{
-//     // blacklists a malicious player
-
-//     address playerAddress = players[playerID].wallet;
-//     if (balcklistedAddress[playerAddress]) revert AddressBlacklisted("address already blacklisted");
-
-//     players[playerID].isBlacklisted = true;
-    
-//     balcklistedAddress[playerAddress] = true;
-
-//     emit PlayerBlacklisted(players[playerID].wallet);
-// }
-
-// // -------------------------------------------------------------
-// // INTERNAL FUNCTIONS
-// // -------------------------------------------------------------
-
-//    function _dealCards() internal {
-//     // TODO
-//     // assuming that the cards are NFTs we can integrate Chainlink's VFR function and deal random tokenIDs to each player and send it to their wallet
-//     // we would save the corresponding 
-//    }
-
-//    function _dealCommunityCards() internal {
-//     // TODO
-//     // assuming that the cards are NFTs we can integrate Chainlink's VFR function deal random cards by a random tokenID to the table
-//    }
-
-//    function _addCommunityCard() internal {
-//     // TODO
-//     // assuming that the cards are NFTs we can integrate Chainlink's VFR function and deal a random card by a random tokenID to the table
-//    }
-
-//    function _revealCards(uint playerID) internal view returns (uint8[] memory cards) {
-//     return players[playerID].cards;
-//    }
-
-//    function _closeTable(uint tableID,uint playerID) internal {
-//     if (tables[tableID].state == TableState.Inactive) revert TableAlreadyClosed(tableID);
-
-//     // close the table
-//     tables[tableID].state = TableState.Inactive;
-//     tables[tableID].winner = players[playerID].wallet;
-
-//     // reset all players to active
-//     uint[] memory playerIDs = tables[tableID].players;
-//     for (uint i = 0; i < playerIDs.length; i++) { 
-//         _setPlayerToActive(playerIDs[i]);
-//     }
-
-//      // TODO send cards back to dealer wallet
-
-//     // for (uint i = 0; i < playerIDs.length; i++) { 
-//     //     IERC1155(tables[tableID].cardsAddress).transferFrom(playerWallets[i],address(this),tokenIDs[i]);
-//     // }
-    
-//     _sendWinnerRewards(tableID,playerID,tables[tableID].totalAmountinPot);
-//     emit TableClosed(tables[tableID].winner,tables[tableID].totalAmountinPot);
-// }
-
-// function _sendWinnerRewards(uint tableID,uint playerID, uint amount) internal {
-
-//      // set totalAmountInPot to 0
-
-//      tables[tableID].totalAmountinPot = 0;
-
-//      IERC20(tables[tableID].token).transferFrom(players[playerID].wallet,tables[tableID].pot,amount);
-
-//      emit RewardsTransferred(players[playerID].wallet,amount);
-// }
-
-// function _setPlayerToActive(uint playerID) internal {
-//     players[playerID].isActivePlayer = true;
-// }
-
-// // -------------------------------------------------------------
-// // CHAINLINK
-// // -------------------------------------------------------------
-
-// function fulfillRandomWords(
-//         uint256 _requestId,
-//         uint256[] memory _randomWords
-//     ) internal override {
-//         // TODO function to get the random numbers
-//     }
-
-//       function getRequestStatus(uint256 _requestId)
-//         external
-//         view
-//         returns (bool fulfilled, uint256[] memory randomWords)
-//     {
-//         require(s_requests[_requestId].exists, "request not found");
-//         RequestStatus memory request = s_requests[_requestId];
-//         return (request.fulfilled, request.randomWords);
-//     }
 
 }
